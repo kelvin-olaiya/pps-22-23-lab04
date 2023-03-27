e7b39cb996d75874f278362503673a3a4384a3f0
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-27 11:41:23 +0200

    chore: implement SecondDegreePolynomial

--- src/main/scala/u04lab/code/SecondDegreePolynomial.scala
+++ src/main/scala/u04lab/code/SecondDegreePolynomial.scala
@@ -1,5 +1,7 @@
 package u04lab.code
 
+import scala.quoted.ToExpr.DoubleToExpr
+
 // Express a second degree polynomial
 // Structure: secondDegree * X^2 + firstDegree * X + constant
 trait SecondDegreePolynomial:
@@ -11,20 +13,18 @@ trait SecondDegreePolynomial:
 
 
 object SecondDegreePolynomial:
-  def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial = ??? // Fill here
+  def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial =
+    SecondDegreePolynomialImpl(secondDegree, firstDegree, constant)
 
-@main def checkComplex(): Unit =
-  val simplePolynomial = SecondDegreePolynomial(1.0, 0, 3)
-  val anotherPolynomial = SecondDegreePolynomial(0.0, 1, 0.0)
-  val fullPolynomial = SecondDegreePolynomial(3.0, 2.0, 5.0)
-  val sum = simplePolynomial + anotherPolynomial
-  println((sum, sum.secondDegree, sum.firstDegree, sum.constant)) // 1.0 * X^2 + 1.0 * X + 3.0
-  val multipleOperations = fullPolynomial - (anotherPolynomial + simplePolynomial)
-  println((multipleOperations, multipleOperations.secondDegree, multipleOperations.firstDegree, multipleOperations.constant)) // 2.0 * X^2 + 1.0 * X + 2.0
+  def unapply(polynomial: SecondDegreePolynomial): scala.Option[(Double, Double, Double)] =
+    scala.Some((polynomial.secondDegree, polynomial.firstDegree, polynomial.constant))
+  private class SecondDegreePolynomialImpl(
+    val secondDegree: Double,
+    val firstDegree: Double,
+    val constant: Double
+  ) extends SecondDegreePolynomial:
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = polynomial match
+      case SecondDegreePolynomial(s, f, c) => SecondDegreePolynomial(s + secondDegree, f + firstDegree, c + constant)
 
-/** Hints:
-  *   - implement SecondDegreePolynomial with a SecondDegreePolynomialImpl class, similar to PersonImpl in slides
-  *   - check that equality and toString do not work
-  *   - use a case class SecondDegreePolynomialImpl instead
-  *   - check equality and toString now
-  */
+    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = polynomial match
+      case SecondDegreePolynomial(s, f, c) => SecondDegreePolynomial(s + secondDegree, f + firstDegree, c + constant)
